81d4640e6be877886e75ac5d95ee4c7090d130a7
MIS-WebDev <webdevaddessa@gmail.com>
2024-08-15 17:33:24 +0800

    updated

--- app/Http/Controllers/InventorySapBackendController.php
+++ app/Http/Controllers/InventorySapBackendController.php
@@ -27,14 +27,23 @@ class InventorySapBackendController extends Controller
             return DB::connection($this->mssqlcon())->table('OSRI');
         }elseif($t == 'gl'){
             return DB::connection($this->mssqlcon())->table('OACT');
+        }elseif($t == 'goodsissuelist'){
+            return DB::connection($this->mssqlcon())->table('OIGE');
         }else{
             return "ERROR WEW!!";
         }
     }
     //LIST OF SAP GETTERS
     public function GettersItems(Request $req){
+       //ARRAY DATA MANIPULATION
+       function Recustomize($DocEntry, $db){
+            return DB::connection($db)->table('IGE1')
+            ->select('DocEntry','ItemCode','Dscription','Quantity','WhsCode')
+            ->where('DocEntry', $DocEntry)->get();
+       }
+       //END
        try {
-            if($req->get == 'items'  ){
+            if($req->get == 'items'){
                 if($req->page || $req){
                     if($req->search){
                         return $this->SapTables('items')
@@ -48,9 +57,7 @@ class InventorySapBackendController extends Controller
                         ->paginate(10);
                     }
                 }
-                
             }elseif($req->get == 'itembywarehouse'){
-                
                 // if($req->search !== 'undefined'){
                 //     return $this->SapTables('itembywarehouse')
                 //     ->select('ItemCode','WhsCode','OnHand','IsCommited','OnOrder')
@@ -69,15 +76,24 @@ class InventorySapBackendController extends Controller
               
             }elseif($req->get == 'availablesn'){
                 return $this->SapTables('availablesn')
-                ->select('IntrSerial','ItemCode','WhsCode')
-                ->where('ItemCode', $req->itemcode)
-                ->where('WhsCode', $req->warehouse)
-                ->where('Status', 0)
-                ->get();
+                    ->select('IntrSerial','ItemCode','WhsCode')
+                    ->where('ItemCode', $req->itemcode)
+                    ->where('WhsCode', $req->warehouse)
+                    ->where('Status', 0)
+                    ->get();
             }elseif($req->get == 'gl'){
                 return $this->SapTables('gl')
                 ->select('AcctCode','AcctName','CurrTotal')
                 ->get();
+            }elseif($req->get == 'goodsissuelist'){
+                return $this->SapTables('goodsissuelist')
+                ->select('DocEntry','DocNum','DocStatus','DocDate','Comments','JrnlMemo')
+                ->orderby('DocDate', 'DESC')
+                ->paginate(1);
+            }elseif($req->get == 'index'){
+                //plucking
+                //?get=index&docentry={}
+                return Recustomize($req->docentry, $this->mssqlcon());
             }else{
                 return "ERROR";
             }
@@ -85,6 +101,7 @@ class InventorySapBackendController extends Controller
          return $e;
        }
      }
+    
      public function send(request $req){
             
             $client = new Client(['timeout' => 300000]);
